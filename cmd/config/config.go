6623ee074075410612d3ab1d053d7cb7a86bba1b
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-15 23:06:49 +0300

    flag task attempt3

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"fmt"
+	"strings"
 )
 
 type Config struct {
@@ -12,40 +13,15 @@ type Config struct {
 
 func ParseFlags(cf *Config) {
 	flag.StringVar(&cf.DefaultAddr, "a", ":8080", "default HTTP-server addres")
+
 	flag.StringVar(&cf.BaseAddr, "b", "http://localhost:8080", "base host addr for short URL response")
 	flag.Parse()
+	hp := strings.Split(cf.DefaultAddr, ":")
+	if len(hp) != 2 {
+		fmt.Println("Need address in a form host:port")
+		return
+	}
+	cf.DefaultAddr = ":" + hp[1]
 	fmt.Println(cf.DefaultAddr)
 	fmt.Println(cf.BaseAddr)
-	//isUrl1, _ := isUrl(cf.DefaultAddr)
-	//isUrl2, _ := isUrl(cf.BaseAddr)
-	/*
-		if !isUrl1 {
-			cf.DefaultAddr = ":8080"
-		}
-
-		if !isUrl2 {
-			cf.BaseAddr = "http://localhost:8080"
-		}*/
-	//fmt.Println(cf.DefaultAddr)
-	//fmt.Println(cf.BaseAddr)
-
 }
-
-/*
-func isURL(str string) (bool, error) {
-	parsedURL, err := url.Parse(str)
-	return err == nil && parsedURL.Scheme != "" && parsedURL.Host != "", err
-	/*url, err := url.ParseRequestURI(str)
-	if err != nil {
-		fmt.Println(err)
-	}
-	if url.Scheme == "" && str[:2] != "://" {
-		str = "http://" + str
-	}
-
-	if url.Host == "" {
-		url.Host = "localhost:8080"
-	}
-	return url, nil
-}
-*/
