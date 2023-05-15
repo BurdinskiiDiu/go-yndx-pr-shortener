98e030536f6192148e55374d947acb180afdacf2
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-15 20:29:58 +0300

    flags are added

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/cmd/config"
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/cmd/store"
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/internal/app/server"
 )
 
 func main() {
+	conf := &config.Config{}
+	config.ParseFlags(conf)
 	uS := store.NewURLStorage()
 	//h := handler.NewRouter(uS)
-	rt := server.NewServer(uS, ":8080")
+	rt := server.NewServer(uS, *conf)
 	//srv := server.NewServer(":8080", h)
 	rt.Run()
 }
