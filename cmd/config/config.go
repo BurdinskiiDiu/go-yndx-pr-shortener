4cbfcf632e8da4103b5d7640629b8012712748f2
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-15 22:08:31 +0300

    flag task

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"net/url"
+)
 
 type Config struct {
 	DefaultAddr string
@@ -11,4 +15,33 @@ func ParseFlags(cf *Config) {
 	flag.StringVar(&cf.DefaultAddr, "a", ":8080", "default HTTP-server addres")
 	flag.StringVar(&cf.BaseAddr, "b", "http://localhost:8080", "base host addr for short URL response")
 	flag.Parse()
+	fmt.Println(cf.DefaultAddr)
+	fmt.Println(cf.BaseAddr)
+	isUrl1, _ := isUrl(cf.DefaultAddr)
+	isUrl2, _ := isUrl(cf.BaseAddr)
+
+	if !isUrl1 {
+		cf.DefaultAddr = ":8080"
+	}
+
+	if !isUrl2 {
+		cf.BaseAddr = "http://localhost:8080"
+	}
+}
+
+func isUrl(str string) (bool, error) {
+	parsedUrl, err := url.Parse(str)
+	return err == nil && parsedUrl.Scheme != "" && parsedUrl.Host != "", err
+	/*url, err := url.ParseRequestURI(str)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if url.Scheme == "" && str[:2] != "://" {
+		str = "http://" + str
+	}
+
+	if url.Host == "" {
+		url.Host = "localhost:8080"
+	}
+	return url, nil*/
 }
