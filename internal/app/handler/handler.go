a517815fb5166dc18a2a713921e6c43bddbfb776
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-18 22:36:15 +0300

    rev attempt7

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"io"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -32,57 +33,55 @@ func shorting() string {
 
 func PostLongURL(uS URLStore, cf config.Config) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			defer r.Body.Close()
-
-			content, err := io.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			longURL := string(content)
-			var shrtURL string
-
-			//shrtURL, err := uS.CreateShortURL(string(content))
-			done := false
-			for !done {
-				shrtURL = shorting()
-				done = uS.PostShortURL(shrtURL, longURL)
-			}
-			/*
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}*/
-
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-			w.Header().Set("Content-Length", strconv.Itoa(len(cf.BaseAddr+"/"+shrtURL)))
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte(cf.BaseAddr + "/" + shrtURL))
-		} else {
+		if r.Method != http.MethodPost {
 			http.Error(w, "bad method", http.StatusBadRequest)
 			return
 		}
+
+		defer r.Body.Close()
+		content, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		longURL := string(content)
+		var shrtURL string
+
+		//shrtURL, err := uS.CreateShortURL(string(content))
+		done := false
+		for !done {
+			shrtURL = shorting()
+			done = uS.PostShortURL(shrtURL, longURL)
+		}
+		/*
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}*/
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Content-Length", strconv.Itoa(len(cf.BaseAddr+"/"+shrtURL)))
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(cf.BaseAddr + "/" + shrtURL))
+
 	})
 }
 
 func GetLongURL(uS URLStore) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			srtURL := r.URL.Path
-			srtURL = srtURL[1:]
-			lngURL, err := uS.GetLongURL(srtURL)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-			w.Header().Set("Location", lngURL)
-			w.WriteHeader(http.StatusTemporaryRedirect)
-			//w.Write([]byte("Location: " + lngURL))
-		} else {
+		if r.Method != http.MethodGet {
 			http.Error(w, "bad method", http.StatusBadRequest)
 			return
 		}
+		srtURL := r.URL.Path
+		srtURL = srtURL[1:]
+		lngURL, err := uS.GetLongURL(srtURL)
+		if err != nil {
+			log.Fatal(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		w.Header().Set("Location", lngURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		//w.Write([]byte("Location: " + lngURL))
 	})
 }
