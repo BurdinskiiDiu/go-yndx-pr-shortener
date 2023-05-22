78a733d8fb41d23b90047d6fb2c7884b5c733ed6
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-22 23:51:35 +0300

    logger is added

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/cmd/config"
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/cmd/store"
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/internal/app/server"
+	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/internal/logger"
 )
 
 func main() {
 	conf := config.GetConfig()
+	err := logger.InitLog(conf, logger.Log)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	uS := store.NewURLStorage()
 	rt := server.NewServer(uS, *conf)
 	rt.Run()
