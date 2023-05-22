1245234226dcf0565e496f672780e1f2409989d3
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-22 17:49:20 +0300

    change handler_test, two func for each handlerFunc

--- internal/app/handler/handler_test.go
+++ internal/app/handler/handler_test.go
@@ -32,11 +32,10 @@ func TestURLShortenerRequest(t *testing.T) {
 		shortURL            string
 	}{
 		{method: http.MethodPost, fun: PostLongURL(uS, conf), expectedCode: http.StatusCreated, target: "/", expectedBody: "", testURL: "http://yandex.practicum.com"},
-		{method: http.MethodGet, fun: GetLongURL(uS, "abcdefj"), expectedCode: http.StatusTemporaryRedirect, expectedBody: "", testURL: "http://yandex.practicum.com"},
 	}
 
 	for i, tc := range testCases {
-		i, tc := i, tc
+		_, tc := i, tc
 		t.Run(tc.method, func(t *testing.T) {
 			r := httptest.NewRequest(tc.method, tc.target, strings.NewReader(tc.testURL))
 			w := httptest.NewRecorder()
@@ -45,20 +44,50 @@ func TestURLShortenerRequest(t *testing.T) {
 
 			result := w.Result()
 			assert.Equal(t, tc.expectedCode, result.StatusCode)
-			if i != 0 {
-				t.Logf("short url for get request test is: " + testCases[0].shortURL)
-				require.Equal(t, tc.testURL, result.Header.Get("Location"))
-				return
-			}
 			getBody, err := io.ReadAll(result.Body)
 			require.NoError(t, err)
 			require.NotEqual(t, "", string(getBody), "empty short url")
 			words := strings.Split(string(getBody), "/")
-			testCases[0].shortURL = words[len(words)-1]
+			tc.shortURL = words[len(words)-1]
 			err = result.Body.Close()
 			require.NoError(t, err)
-			testCases[1].shortURL = testCases[0].shortURL
-			testCases[1].target = "/" + testCases[1].shortURL
 		})
 	}
+}
+
+func TestGetlongURLRequest(t *testing.T) {
+	uS := store.NewURLStorage()
+	uS.URLStr["abcdefj"] = "http://yandex.practicum.com"
+
+	testCase := []struct {
+		name                string
+		method              string
+		fun                 http.HandlerFunc
+		expectedCode        int
+		target              string
+		expectedBody        string
+		expectedContentType string
+		testURL             string
+		shortURL            string
+	}{
+		{method: http.MethodGet, fun: GetLongURL(uS, "abcdefj"), expectedCode: http.StatusTemporaryRedirect, target: "/", expectedBody: "", testURL: "http://yandex.practicum.com", shortURL: "abcdefj"},
+	}
+
+	for i, tc := range testCase {
+		_, tc := i, tc
+		t.Run(tc.method, func(t *testing.T) {
+			tc.target += tc.target + tc.shortURL
+			r := httptest.NewRequest(tc.method, tc.target, nil)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(tc.fun)
+			h(w, r)
+
+			result := w.Result()
+			assert.Equal(t, tc.expectedCode, result.StatusCode)
+			t.Logf("short url for get request test is: " + tc.shortURL)
+			require.Equal(t, tc.testURL, result.Header.Get("Location"))
+			return
+		})
+	}
+
 }
