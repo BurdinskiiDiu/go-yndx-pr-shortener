af8aa17cfcfb1f0c863924b0f953972e9fc93b3c
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-24 17:23:49 +0300

    iter7 attmpt3

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/cmd/config"
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/internal/logger"
+	"go.uber.org/zap"
 )
 
 type URLStore interface {
@@ -88,28 +89,36 @@ func PostURLApi(uS URLStore, cf config.Config) http.HandlerFunc {
 		if err != nil {
 			logger.Log.Error(err.Error())
 		}
+		logger.Log.Info("got postApi message", zap.String("body", buf.String()))
 
 		var urlReq URLReq
-
 		if err := json.Unmarshal(buf.Bytes(), &urlReq); err != nil {
 			logger.Log.Error(err.Error())
 			return
 		}
+		logger.Log.Info("unmarshaled url from postApi message", zap.String("longURL", urlReq.URL))
 
 		shrtURL, err := CreateShortURL(uS, urlReq.URL)
 		if err != nil {
 			logger.Log.Error(err.Error())
 			return
 		}
+		logger.Log.Info("short url", zap.String("shortURL", shrtURL))
 		var urlResp URLResp
 		urlResp.Result = cf.BaseAddr + "/" + shrtURL
-		resp, err := json.MarshalIndent(urlResp.Result, "", "   ")
+		resp, err := json.MarshalIndent(urlResp, "", " ")
 		if err != nil {
 			logger.Log.Error(err.Error())
 			return
-		}
+		} /*
+			respString := string(resp)
+			respString = respString[1:]
+			respString = respString[:len(respString)-1]
+			fmt.Println(string(resp))
+			fmt.Println(string(respString))*/
+		logger.Log.Info("response for postApi request", zap.String("response", string(resp)))
 		w.Header().Set("Content-Type", "application/json")
-		//w.Header().Set("Content-Length", ))
+		w.Header().Set("Content-Length", string(resp))
 		w.WriteHeader(http.StatusCreated)
 		w.Write(resp)
 	})
