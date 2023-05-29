1cc4e70ea1d4f6c146df1317fcc0710828355325
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-05-29 12:08:50 +0300

    itr8 attmpt12

--- cmd/gzip/gzip.go
+++ cmd/gzip/gzip.go
@@ -11,32 +11,31 @@ import (
 )
 
 type compressWriter struct {
-	http.ResponseWriter
+	w  http.ResponseWriter
 	zw *gzip.Writer
 }
 
 func newCompressWriter(w http.ResponseWriter) *compressWriter {
 	return &compressWriter{
-		ResponseWriter: w,
-		zw:             gzip.NewWriter(w),
+		w:  w,
+		zw: gzip.NewWriter(w),
 	}
 }
 
-/*
 func (c *compressWriter) Header() http.Header {
 	return c.w.Header()
-}*/
-/*
+}
+
 func (c *compressWriter) Write(p []byte) (int, error) {
 	return c.zw.Write(p)
-}*/
-/*
+}
+
 func (c *compressWriter) WriteHeader(statusCode int) {
 	if statusCode < 300 {
 		c.w.Header().Set("Content-Encoding", "gzip")
 	}
 	c.w.WriteHeader(statusCode)
-}*/
+}
 
 func (c *compressWriter) Close() error {
 	return c.zw.Close()
