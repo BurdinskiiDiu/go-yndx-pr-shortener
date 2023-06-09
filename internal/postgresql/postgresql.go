6561058016bf1cd8f21153d8b7e78500bf3129d3
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-06-09 22:45:14 +0300

    iter11 attmpt23

--- internal/postgresql/postgresql.go
+++ internal/postgresql/postgresql.go
@@ -59,7 +59,7 @@ func (cDBS *ClientDBStruct) Create() error {
 	ctx, cansel := context.WithTimeout(cDBS.ctx, 5*time.Second)
 	defer cansel()
 
-	res, err := cDBS.db.ExecContext(ctx /*query*/, `CREATE TABLE IF NOT EXISTS urlstorage(short_url text, long_url text)`)
+	res, err := cDBS.db.ExecContext(ctx /*query*/, `CREATE TABLE IF NOT EXISTS urlstorage("short_url" TEXT, "long_url" TEXT)`)
 	if err != nil {
 		cDBS.logger.Error("creating db method, error while creating new table", zap.Error(err))
 		return err
