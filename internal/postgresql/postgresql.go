fbaa9de4f7e76c4fb77eae170fb7830f95f4ce5e
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-06-12 23:03:18 +0300

    iter11 attmpt40

--- internal/postgresql/postgresql.go
+++ internal/postgresql/postgresql.go
@@ -58,7 +58,7 @@ func (cDBS *ClientDBStruct) Create() error {
 	defer cansel()
 
 	//res, err := cDBS.db.ExecContext(ctx /*query*/, `CREATE TABLE IF NOT EXISTS urlstorage("id" SERIAL PRIMARY KEY, "short_url" TEXT, "long_url" TEXT)`)
-	res, err := cDBS.db.ExecContext(ctx /*query*/, `CREATE TABLE IF NOT EXISTS urlstorage("id" INT, "short_url" TEXT, "long_url" TEXT)`)
+	res, err := cDBS.db.ExecContext(ctx /*query*/, `CREATE TABLE IF NOT EXISTS urlstorage("id" INTEGER, "short_url" TEXT, "long_url" TEXT)`)
 	//res, err := cDBS.db.Exec( /*query*/ `CREATE TABLE IF NOT EXISTS urlstorage("short_url" TEXT, "long_url" TEXT)`)
 
 	if err != nil {
