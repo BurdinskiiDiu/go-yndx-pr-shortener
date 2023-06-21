84e27574aa104d3efae6a472d9469ad8664bbaa5
Dmitry Burdinskii <byrguy2108@mail.ru>
2023-06-21 23:23:43 +0300

    iter13 fixing 5

--- internal/postgresql/postgresql.go
+++ internal/postgresql/postgresql.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"strings"
 	"time"
 
 	"github.com/BurdinskiiDiu/go-yndx-pr-shortener.git/internal/config"
@@ -168,7 +169,7 @@ func (cDBS *ClientDBStruct) PostShortURL(shortURL, longURL string, uuid int32) (
 	var shURL, lnURL string
 	err := cDBS.db.QueryRow(ctx, `SELECT long_url FROM urlstorage WHERE short_url=$1`, shortURL).Scan(&lnURL)
 	if err != nil {
-		if !errors.Is(err, sql.ErrNoRows) {
+		if !strings.Contains(err.Error(), "no rows in result set") {
 			return "", errors.New("postShortURL db method, err while selecting short url: " + err.Error())
 		}
 	}
